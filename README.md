2a685f96eab881ec6b85208a610117fe71c5f9e1
Priyansi <priyanhiman@gmail.com>
2020-10-15 05:59:36 +0530

    added iterative traversing for in and pre order

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Data Structures and Algorithms in C as part of my coursework. Data Structures in
 - Linked Lists
 - Stacks
 - Queues
+- Trees
+
+Common algorithms that use these data structures are also given.
--- tree/binary_tree.c
+++ tree/binary_tree.c
@@ -7,6 +7,44 @@ struct node {
     struct node* right;
 };
 
+struct node_stack {
+    struct node* data;
+    struct node_stack* next;
+};
+
+typedef struct {
+    struct node_stack* top;
+} STACK;
+
+int push(STACK* s, struct node* val) {
+    if (val == NULL) {
+        return 1;
+    }
+    struct node_stack* curr;
+    curr = (struct node_stack*)malloc(sizeof(struct node_stack));
+    if (curr == NULL) {
+        return 1;
+    }
+    curr->data = val;
+    curr->next = NULL;
+    curr->next = s->top;
+    s->top = curr;
+    return 0;
+}
+
+int pop(STACK* s, struct node** del) {
+    if (s->top == NULL) {
+        return 1;
+    }
+    *del = s->top->data;
+    s->top = s->top->next;
+    return 0;
+}
+
+int is_empty(STACK s) {
+    return s.top == NULL;
+}
+
 struct node* create(int val) {
     struct node* curr = (struct node*)malloc(sizeof(struct node));
     curr->data = val;
@@ -14,27 +52,65 @@ struct node* create(int val) {
     return curr;
 }
 
-void preorder_traverse(struct node* root) {
+void preorder_traverse_recr(struct node* root) {
     if (root != NULL) {
-        printf("%d->", root->data);
-        preorder_traverse(root->left);
-        preorder_traverse(root->right);
+        printf("%d ", root->data);
+        preorder_traverse_recr(root->left);
+        preorder_traverse_recr(root->right);
     }
 }
 
-void inorder_traverse(struct node* root) {
-    if (root != NULL) {
-        inorder_traverse(root->left);
-        printf("%d->", root->data);
-        inorder_traverse(root->right);
+void preorder_traverse_iter(struct node* root) {
+    STACK s;
+    s.top = NULL;
+    struct node* del;
+    push(&s, root);
+    while (!is_empty(s)) {
+        pop(&s, &del);
+        printf("%d ", del->data);
+        if (del->right != NULL) {  // right is pushed first because left should be popped first and therefore on top
+            push(&s, del->right);
+        }
+        if (del->left != NULL) {
+            push(&s, del->left);
+        }
     }
 }
 
-void postorder_traverse(struct node* root) {
+void inorder_traverse_recr(struct node* root) {
     if (root != NULL) {
-        postorder_traverse(root->left);
-        postorder_traverse(root->right);
-        printf("%d->", root->data);
+        inorder_traverse_recr(root->left);
+        printf("%d ", root->data);
+        inorder_traverse_recr(root->right);
+    }
+}
+
+void inorder_traverse_iter(struct node* root) {
+    STACK s;
+    s.top = NULL;
+    struct node* curr_node = root;
+    struct node* del;
+    // start with the leftmost element
+    while (curr_node != NULL) {
+        push(&s, curr_node);
+        curr_node = curr_node->left;
+    }
+    while (!is_empty(s)) {
+        pop(&s, &del);  // pop the leftmost node
+        printf("%d ", del->data);
+        curr_node = del->right;      // if right doesn't exist, repeat loop to pop parent
+        while (curr_node != NULL) {  // if right exists, it becomes the curr (root node) and again goes on left
+            push(&s, curr_node);
+            curr_node = curr_node->left;
+        }
+    }
+}
+
+void postorder_traverse_recr(struct node* root) {
+    if (root != NULL) {
+        postorder_traverse_recr(root->left);
+        postorder_traverse_recr(root->right);
+        printf("%d ", root->data);
     }
 }
 
@@ -49,11 +125,10 @@ int main(int argc, char* argv[]) {
     root->right->left = create(8);
     root->right->right = create(9);
     root->right->right->left = create(10);
-    inorder_traverse(root);
+    inorder_traverse_recr(root);
     printf("\n");
-    preorder_traverse(root);
+    inorder_traverse_iter(root);
     printf("\n");
-    postorder_traverse(root);
-    printf("\n");
+
     return 0;
 }
