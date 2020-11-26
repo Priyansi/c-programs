0d2201c1adfde257bfed1d4328bc432e8ee92775
priyansi <priyanhiman@gmail.com>
2020-11-26 21:38:07 +0530

    corrected quick sort

--- sorting/quick_sort.c
+++ sorting/quick_sort.c
@@ -1,50 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-void merge(int* arr, int lower_ind, int mid_ind, int upper_ind) {
-    int i = lower_ind, j = mid_ind + 1, k = 0;
-    int* temp = (int*)malloc((upper_ind - lower_ind + 1) * sizeof(int));
-    while (i <= mid_ind && j <= upper_ind) {
-        if (arr[i] < arr[j]) {
-            temp[k++] = arr[i];
-            ++i;
-        } else {
-            temp[k++] = arr[j];
-            ++j;
+int partition(int* arr, int lower_ind, int upper_ind) {
+    int l = lower_ind;
+    int u = upper_ind;
+    int pivot = arr[lower_ind];
+    int temp;
+    while (1) {
+        while (l <= upper_ind && arr[l] <= pivot) {
+            ++l;
         }
+        while (u >= lower_ind && arr[u] > pivot) {
+            --u;
+        }
+        if (l >= u) {
+            break;
+        }
+        temp = arr[l];
+        arr[l] = arr[u];
+        arr[u] = temp;
     }
-    while (i <= mid_ind) {
-        temp[k++] = arr[i];
-        ++i;
-    }
-    while (j <= upper_ind) {
-        temp[k++] = arr[j];
-        ++j;
-    }
-    for (int i = 0; i < (upper_ind - lower_ind + 1); ++i) {
-        arr[lower_ind + i] = temp[i];
-    }
+    temp = arr[lower_ind];
+    arr[lower_ind] = arr[u];
+    arr[u] = temp;
+    return u;
 }
 
-void merge_sort(int* arr, int lower_ind, int upper_ind) {
-    int mid_ind = (lower_ind + upper_ind) / 2;
+void quick_sort(int* arr, int lower_ind, int upper_ind) {
     if (lower_ind < upper_ind) {
-        merge_sort(arr, lower_ind, mid_ind);
-        merge_sort(arr, mid_ind + 1, upper_ind);
-        merge(arr, lower_ind, mid_ind, upper_ind);
+        int pivot_ind = partition(arr, lower_ind, upper_ind);
+        quick_sort(arr, lower_ind, pivot_ind - 1);
+        quick_sort(arr, pivot_ind + 1, upper_ind);
     }
 }
 
-int main(int argc, char* argv[]) {
+int main() {
     int arr[] = {3, 5, 8, 9, 4};
+
+    printf("Unsorted array :\n");
     for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i) {
         printf("%d ", arr[i]);
     }
     printf("\n");
-    merge_sort(arr, 0, (sizeof(arr) / sizeof(arr[0])) - 1);
+
+    printf("Sorted array :\n");
+    quick_sort(arr, 0, (sizeof(arr) / sizeof(arr[0])) - 1);
     for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i) {
         printf("%d ", arr[i]);
     }
+    printf("\n");
     return 0;
 }
